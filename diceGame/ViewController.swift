eb3615d70b35e6aaa67c4a7a84b1a926f5715756
Vitor Henrique <vhenriquebm@gmail.com>
2022-01-04 11:03:24 -0300

    Initial Commit

---
+++ diceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diceGame
+//
+//  Created by Vitor Henrique Barreiro Marinho on 04/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
