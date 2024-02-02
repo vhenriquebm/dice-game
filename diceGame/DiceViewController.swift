4336d05af41cfacbd62ec92479194adf24c4c3b4
vhenriquebm <vhenriquebm@gmail.com>
2024-02-02 12:10:20 -0300

    Implemented project fixes

--- diceGame/DiceViewController.swift
+++ diceGame/DiceViewController.swift
@@ -7,30 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    
+class DiceViewController: UIViewController {
     @IBOutlet weak var dice1: UIImageView!
-    
-    
     @IBOutlet weak var dice2: UIImageView!
-    
-    
-    @IBAction func play(_ sender: Any) {
-        
-        
-        dice1.image = [#imageLiteral(resourceName: "1"), #imageLiteral(resourceName: "2"), #imageLiteral(resourceName: "3"), #imageLiteral(resourceName: "4"), #imageLiteral(resourceName: "5"), #imageLiteral(resourceName: "6") ] [Int.random(in: 0...5)]
-        
-        
-        dice2.image = [#imageLiteral(resourceName: "1"), #imageLiteral(resourceName: "2"), #imageLiteral(resourceName: "3"), #imageLiteral(resourceName: "4"), #imageLiteral(resourceName: "5"), #imageLiteral(resourceName: "6") ] [Int.random(in: 0...5) ]
-    
-    }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func play(_ sender: Any) {
+        dice1.image = [#imageLiteral(resourceName: "1"), #imageLiteral(resourceName: "2"), #imageLiteral(resourceName: "3"), #imageLiteral(resourceName: "4"), #imageLiteral(resourceName: "5"), #imageLiteral(resourceName: "6") ] [Int.random(in: 0...5)]
+        
+        dice2.image = [#imageLiteral(resourceName: "1"), #imageLiteral(resourceName: "2"), #imageLiteral(resourceName: "3"), #imageLiteral(resourceName: "4"), #imageLiteral(resourceName: "5"), #imageLiteral(resourceName: "6") ] [Int.random(in: 0...5) ]
+    }
 }
 
